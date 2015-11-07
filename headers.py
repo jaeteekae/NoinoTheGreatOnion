b43e8fc58f8d6f8bfb9d84ac9dbc97d3fe06e967
jaeteekae <julia.knight@tufts.edu>
2015-11-07 17:38:16 -0500

    added header r

--- headers.py
+++ headers.py
@@ -2,11 +2,19 @@ from parse import *
 
 
 class HeaderK:
-    # returns msg + header containing key
-    def add(self, msg, key):
+    # returns header containing key + msg
+    def add(self, key, msg):
         return "Key: " + key + "\nMessage: " + msg
 
     # returns tuple of key and msg
     def extract(self, msg):
         return parse("Key: {}\nMessage: {}", msg)
 
+class HeaderR:
+    # returns header containing IP address & port of next hop + msg
+    def add(self, ip, port, msg):
+        return "IP: " + ip + "\nPort: " + port + "\nMessage: " + msg
+
+    # returns tuple of IP, port, and msg
+    def extract(self, msg):
+        return parse("IP: {}\nPort: {}\nMessage: {}", msg)
