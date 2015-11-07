f6105777d48a1231f27eba908f21bbe346189273
Eli Boninger <eli.boninger@tufts.edu>
2015-11-07 17:41:31 -0500

    Added scaffolding for path finding

--- server.py
+++ server.py
@@ -8,13 +8,32 @@ import socket
 import select
 import time
 import sys
+import random
 from headers import HeaderK
+from BaseHTTPServer import BaseHTTPRequestHandler
+from StringIO import StringIO
 
 # Changing the buffer_size and delay, you can improve the speed and bandwidth.
 # But when buffer get to high or delay go too down, you can broke things
 buffer_size = 4096
 delay = 0.0001
 forward_to = ('smtp.zaz.ufsk.br', 25)
+
+
+
+
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, request_text):
+        self.rfile = StringIO(request_text)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+
+    def send_error(self, code, message):
+        self.error_code = code
+        self.error_message = message
+
+
 
 class Forward:
     def __init__(self):
@@ -54,7 +73,7 @@ class TheServer:
                     self.on_close()
                     break
                 else:
-                    self.on_recv()
+                    self.on_recv(self.s)
 
     def on_accept(self):
         clientsock, clientaddr = self.server.accept()
@@ -65,22 +84,31 @@ class TheServer:
         print self.s.getpeername(), "has disconnected"
         #remove objects from input_list
         self.input_list.remove(self.s)
-        self.input_list.remove(self.channel[self.s])
-        out = self.channel[self.s]
+        #self.input_list.remove(self.channel[self.s])
+        #out = self.channel[self.s]
         # close the connection with client
-        self.channel[out].close()  # equivalent to do self.s.close()
+        #self.channel[out].close()  # equivalent to do self.s.close()
         # close the connection with remote server
-        self.channel[self.s].close()
+        #self.channel[self.s].close()
         # delete both objects from channel dict
-        del self.channel[out]
-        del self.channel[self.s]
+        #del self.channel[out]
+        #del self.channel[self.s]
 
-    def on_recv(self):
+    def on_recv(self, sockfd):
         data = self.data
         # here we can parse and/or modify the data before send forward
-        print "here"
+        request = HTTPRequest(data)
         print data
-        #self.channel[self.s].send(data)
+        if request.command == "GET":
+            self.create_path(data, sockfd)
+
+    def create_path(self, data, sockfd):
+        indexes = set(range(len(self.input_list)))
+        bob = indexes.remove(random.choice(list(indexes)))
+        print bob
+
+        print sockfd
+
 
 if __name__ == '__main__':
         server = TheServer('', 9099)
@@ -89,6 +117,3 @@ if __name__ == '__main__':
         except KeyboardInterrupt:
             print "Ctrl C - Stopping server"
             sys.exit(1)
-
-
-
