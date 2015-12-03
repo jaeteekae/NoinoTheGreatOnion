8575527edc58c6cbebcadcb6bc8b71d57b672959
Eli Boninger <eli.boninger@tufts.edu>
2015-12-02 23:28:52 -0500

    Return decryption

--- client.py
+++ client.py
@@ -113,6 +113,9 @@ class TheClient:
         self.temp_connection_no_response(n2[0], n2[1], str(m2to3))
         self.temp_connection_no_response(n1[0], n1[1], str(m1to2))
         self.response = self.temp_connection_with_response(n1[0], n1[1], str(enc_msg))
+        self.response = n3_key.decrypt((self.response,), 32)
+        self.response = n2_key.decrypt((self.response,), 32)
+        self.response = n1_key.decrypt((self.response,), 32)
         print str(self.response)
         self.temp_connection_no_response(self.host, self.port, HeaderN.add(nonce))
         
